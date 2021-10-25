5e6a0b891f7b8c13be086d7839df9ea6ea26726d
naufalanargya <naufalanargyar@gmail.com>
2021-10-25 07:22:06 +0700

    aaaa

---
+++ ptt.js
@@ -0,0 +1,9 @@
+<button onclick="myFunction()">Click me</button>
+
+<p id="button"></p>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+</script>
